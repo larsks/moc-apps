abaeb579d4e992a42cac20a8e5c686a204af1a59
Lars Kellogg-Stedman <lars@redhat.com>
2022-03-01 19:52:44 -0500

    Only validate trees with modified files
    Update ci/validate-manifests.sh so that by default it will only validate
    overlays that contain files modified in the current commit. The '-a' flag
    restores the original behavior of validating all discovered overlays.
    
    This change makes the script more useful in a pre-push hook (where
    otherwise the time required to build all the overlays can be annoying).

--- ci/validate-manifests.sh
+++ ci/validate-manifests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-: ${KUSTOMIZE:=kustomize}
-: ${KUBEVAL:=kubeval}
-: ${CONFTEST:=conftest}
-: ${SCHEMA_LOCATION:=https://raw.githubusercontent.com/CCI-MOC/openshift-schemas/master/schemas/}
+: "${KUSTOMIZE:=kustomize}"
+: "${KUBEVAL:=kubeval}"
+: "${CONFTEST:=conftest}"
+: "${SCHEMA_LOCATION:=https://raw.githubusercontent.com/CCI-MOC/openshift-schemas/master/schemas/}"
+
+PROCESS_ALL_TREES=0
 
 find_overlays() {
     find * -type f -regex '.*/overlays/.*/kustomization.yaml' -printf '%h\n'
@@ -15,17 +17,24 @@ okay() {
 
 fail() {
     echo "$(tput setaf 1)${1}:failed$(tput sgr0)"
-    [[ -s "$tmpdir/stdout" ]] && { echo; cat $tmpdir/stdout; }
-    [[ -s "$tmpdir/stderr" ]] && { echo; cat $tmpdir/stderr; }
+    [[ -s "$tmpdir/stdout" ]] && { echo; cat "$tmpdir/stdout"; }
+    [[ -s "$tmpdir/stderr" ]] && { echo; cat "$tmpdir/stderr"; }
     exit 1
 }
 
-while getopts vis: ch; do
+while getopts ais: ch; do
     case $ch in
+        (a) PROCESS_ALL_TREES=1
+            ;;
+
         (i) IGNORE_MISSING_SCHEMAS=1
             ;;
 
         (s) SCHEMA_LOCATION=$OPTARG
+            ;;
+
+        (*) echo "invalid flag: $ch" >&2
+            exit 2
             ;;
     esac
 done
@@ -34,11 +43,17 @@ shift $(( OPTIND - 1 ))
 tmpdir=$(mktemp -d buildXXXXXX)
 trap "rm -rf $tmpdir" EXIT
 
+git diff-index HEAD^ --name-only --diff-filter ACMR > "$tmpdir/files-in-commit"
+
 for overlay in $(find_overlays); do
-    > $tmpdir/stdout
+    : > "$tmpdir/stdout"
+
+    if [[ $PROCESS_ALL_TREES != 1 ]]; then
+        grep "$overlay" "$tmpdir/files-in-commit" || continue
+    fi
 
     echo -n "$overlay "
-    if $KUSTOMIZE build $overlay > $tmpdir/manifests.yaml 2> $tmpdir/stderr; then
+    if $KUSTOMIZE build "$overlay" > "$tmpdir/manifests.yaml" 2> "$tmpdir/stderr"; then
         okay build
     else
         fail build
@@ -47,7 +62,7 @@ for overlay in $(find_overlays); do
     if $KUBEVAL --strict \
         ${IGNORE_MISSING_SCHEMAS:+--ignore-missing-schemas} \
         ${SCHEMA_LOCATION:+--additional-schema-locations $SCHEMA_LOCATION} \
-            $tmpdir/manifests.yaml > $tmpdir/stdout 2> $tmpdir/stderr; then
+            "$tmpdir/manifests.yaml" > "$tmpdir/stdout" 2> "$tmpdir/stderr"; then
         okay schema
     else
         fail schema
